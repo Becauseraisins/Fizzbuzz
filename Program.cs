85a6236aa0bf60ef0e73517d593c3ea3c159a766
Becauseraisins <79813812+Becauseraisins@users.noreply.github.com>
2021-04-04 09:30:06 +1000

    try that again

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fuzzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Random rand = new Random();
+            int randomNumber = rand.Next(100);
+            bool fuzz = randomNumber % 3 ==0;
+            bool buzz = randomNumber % 5 ==0;
+        //i had no idea how to seperate writing out the number from writing fuzz and buzz so i just put them in a completely seperate path
+        if(fuzz||buzz){
+            if(fuzz){
+                Console.Write("fuzz");
+            }
+            if(buzz){
+                Console.Write("buzz");
+            }
+        }
+        else{
+            Console.WriteLine(randomNumber);
+        }
+
+        }
+    }
+}
